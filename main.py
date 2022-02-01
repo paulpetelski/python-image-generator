3a17a080d2249d601552edcb7d92c85c2480df72
paulpetelski <petelskipaul@gmail.com>
2022-02-01 08:23:49 -0800

    Update

---
+++ main.py
@@ -0,0 +1,13 @@
+# Image Generator
+from PIL import Image
+from IPython.display import display
+import random
+import json
+
+# Traits for each image and weight of each trait
+background = ["Red", "Blue"]
+background_weights = [50, 50]
+
+circle = ["Yellow", "Cyan"]
+circle_weights = [50,50]
+
